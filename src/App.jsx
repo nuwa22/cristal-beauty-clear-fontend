808a4408aa8e096686518fa5c2bfc88fbddd8c1c
Suresh <snt4800nuwa@gmail.com>
2025-04-06 16:35:54 +0530

    install tailwind css✏️

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header'
+import ProductCard from './components/productCart'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-full h-screen bg-pink-200'>
+      <div className='w-[500px] flex flex-col justify-center items-center  h-[500px] bg-gray-200 relative'>
+        <div className='w-[90px] h-[90px] bg-red-500'></div>
+        <div className='w-[90px] h-[90px] bg-yellow-500 absolute right-[50px] top-[50px]'></div>
+        <div className='w-[90px] h-[90px] bg-blue-500 fixed right-[50px] bottom-[50px]'></div>
+        <div className='w-[90px] h-[90px] bg-green-500'></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+    return(
+        <header className="w-full bg-blue-500 h-[50px] text-white">
+            <h1>My Store</h1>
+            
+        </header>
+    )
+}
